ee30c71f8e5bc3204d043cfa929ed402ab404e80
Riccardo Fano <rcr.fano@gmail.com>
2024-05-23 20:33:24 +0200

    Stop loading the images the placeholder server is so slow

--- resources/views/components/dino-card.blade.php
+++ resources/views/components/dino-card.blade.php
@@ -1,8 +1,8 @@
 @props(['dino'])
 
 <div class="min-h-60 flex flex-col rounded-lg bg-white/10 p-4">
-    <div class="mx-auto h-28 w-28">
-        <img class="h-full w-full" src="{{ $dino->discord_url }}" alt="">
+    <div class="mx-auto h-28 w-28 bg-blue-800">
+        {{-- <img class="h-full w-full" src="{{ $dino->discord_url }}" alt=""> --}}
     </div>
 
     <h3 class="mt-4 text-center text-lg font-bold">{{ $dino->name }}</h3>
