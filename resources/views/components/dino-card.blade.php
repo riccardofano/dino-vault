9f4669cfaa00641deee7ff389b89f041d5bc9957
Riccardo Fano <rcr.fano@gmail.com>
2024-05-25 11:39:16 +0200

    Show dino parts in card

--- resources/views/components/dino-card.blade.php
+++ resources/views/components/dino-card.blade.php
@@ -1,8 +1,12 @@
 @props(['dino'])
 
 <div class="min-h-60 flex flex-col rounded-lg bg-white/10 p-4">
-    <div class="mx-auto h-28 w-28 bg-blue-800">
+    <div class="relative mx-auto h-28 w-28">
         {{-- <img class="h-full w-full" src="{{ $dino->discord_url }}" alt=""> --}}
+
+        <x-dino-part filename="{{ $dino->body }}" />
+        <x-dino-part filename="{{ $dino->mouth }}" />
+        <x-dino-part filename="{{ $dino->eyes }}" />
     </div>
 
     <h3 class="mt-4 text-center text-lg font-bold">{{ $dino->name }}</h3>
---
+++ resources/views/components/dino-part.blade.php
@@ -0,0 +1,3 @@
+@props(['filename'])
+
+<img class="absolute inset-0" src="{{ Vite::asset('resources/assets/fragments/' . $filename) }}" alt="">
