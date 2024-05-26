2876abd30c3b68de3a04986bb954e1aff0adc7d0
Riccardo Fano <rcr.fano@gmail.com>
2024-05-26 10:25:49 +0200

    Add ability to covet and shun dino

--- app/Http/Controllers/DinoController.php
+++ app/Http/Controllers/DinoController.php
@@ -14,22 +14,60 @@ class DinoController extends Controller
         return view('dino', ['dino' => $dino]);
     }
 
-    public function favourite(Dino $dino)
+    public function covet(int $dino)
+    {
+        DinoController::replaceCovetOrShun($dino, 'COVET');
+        return redirect('/dinos/' . $dino);
+    }
+
+    public function shun(int $dino)
+    {
+        DinoController::replaceCovetOrShun($dino, 'SHUN');
+        return redirect('/dinos/' . $dino);
+    }
+
+    public function favourite(int $dino)
     {
         /** @var \App\Models\User */
         $user = Auth::user();
 
-        $transaction = $user->favouritedDinos()->where('dino_id', '=', $dino->id)->pluck('dino_transaction.id');
+        $transaction = $user->favouritedDinos()
+            ->where('dino_id', '=', $dino)
+            ->pluck('dino_transaction.id');
 
-        if ($transaction) {
+        if (!$transaction->isEmpty()) {
             DinoTransaction::destroy($transaction);
         } else {
             $user->dinoTransactions()->create([
-                'dino_id' => $dino->id,
+                'dino_id' => $dino,
                 'type' => 'FAVOURITE'
             ]);
         }
 
-        return redirect('/dinos/' . $dino->id);
+        return redirect('/dinos/' . $dino);
+    }
+
+    static private function replaceCovetOrShun(int $dino, string $transactionType)
+    {
+        /** @var \App\Models\User */
+        $user = Auth::user();
+
+        $transaction = $user->dinoTransactions()
+            ->where('dino_id', '=', $dino)
+            ->where(function ($query) {
+                $query
+                    ->where('type', '=', 'COVET')
+                    ->orWhere('type', '=', 'SHUN');
+            })
+            ->pluck('dino_transaction.id');
+
+        if (!$transaction->isEmpty()) {
+            DinoTransaction::destroy($transaction);
+        }
+
+        $user->dinoTransactions()->create([
+            'dino_id' => $dino,
+            'type' => $transactionType
+        ]);
     }
 }
